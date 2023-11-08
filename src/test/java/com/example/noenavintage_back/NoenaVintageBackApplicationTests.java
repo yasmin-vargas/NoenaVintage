8227f01b61007050ede7136186fb305cd06cee7d
NoenaVintage <ni_varro@yahoo.es>
2023-11-09 00:11:31 +0100

    Initial commit

---
+++ src/test/java/com/example/noenavintage_back/NoenaVintageBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noenavintage_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoenaVintageBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
