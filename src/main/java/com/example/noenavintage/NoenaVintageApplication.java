980ebb7b3af9d05f0b674ee3d8d49b16b1226af7
NoenaVintage <ni_varro@yahoo.es>
2023-11-27 01:16:56 +0100

    Name changed

--- src/main/java/com/example/noenavintage/NoenaVintageApplication.java
+++ src/main/java/com/example/noenavintage/NoenaVintageApplication.java
@@ -1,10 +1,9 @@
 package com.example.noenavintage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootApplication
-public class NoenaVintageBackApplication {
+public class NoenaVintageApplication {
     public static void main(String[] args) {
         SpringApplication.run(NoenaVintageBackApplication.class, args);
     }
