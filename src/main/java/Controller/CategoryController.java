ade425daced3f37b8b604c742ac894c5c1e16dff
Yasmin Vargas <91469889+yasmin-vargas@users.noreply.github.com>
2023-11-28 05:09:26 +0100

    Add files via upload

--- src/main/java/Controller/CategoryController.java
+++ src/main/java/Controller/CategoryController.java
@@ -3,6 +3,7 @@ import org.springframework.web.bind.annotation.*;
 import Model.Category;
 import Repository.CategoryData;
 import java.util.List;
+import java.util.ArrayList;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,26 +21,32 @@ public class CategoryController {
     }
 
     // Endpoint to get all categories
-    @GetMapping
+    @GetMapping("/{GetAllCategories}")
     public List<Category> getAllCategories() {
         List<Category> categoryList = categoryData.findAll();
         return categoryList;
     }
     // Endpoint to get a specific category by ID
-    @GetMapping("/{id}")
+    @GetMapping("/{getCategoryByID}")
     public ResponseEntity<Category> getCategoryById(@PathVariable Long categoryID) {
         return categoryData.findByCategoryID(categoryID)
                 .map(category -> ResponseEntity.ok(category))
                 .orElse(ResponseEntity.notFound().build());
     }
+    // Endpoint to get categories based on parentCategory
+    @GetMapping("/parent/{parentCategory}")
+    public List<Category> getCategoriesByParentCategory(@PathVariable String parentCategory) {
+        List<Category> categories = categoryData.findByParentCategory(parentCategory);
+        return categories;
+    }
     // Endpoint to create a new category
-    @PostMapping
+    @PostMapping("/{postCategory}")
     public ResponseEntity<Category> createCategory(@RequestBody Category category) {
         Category createdCategory = categoryData.save(category);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdCategory);
     }
     // Endpoint to update an existing category
-    @PutMapping("/{categoryID}")
+    @PutMapping("/{updateCategory}")
     public ResponseEntity<Category> updateCategory(@PathVariable Long categoryID, @RequestBody Category category) {
         if (!categoryData.existsById(categoryID)) {
             return ResponseEntity.notFound().build();
@@ -50,7 +57,7 @@ public class CategoryController {
     }
 
     // Endpoint to delete a category
-    @DeleteMapping("/{categoryID}")
+    @DeleteMapping("/{deleteCategory}")
     public ResponseEntity<Void> deleteCategory(@PathVariable Long categoryID) {
         if (categoryData.existsById(categoryID)) {
             categoryData.deleteById(categoryID);
---
+++ src/main/java/Controller/StockItemController.java
@@ -0,0 +1,68 @@
+package Controller;
+import Model.StockItem;
+import Repository.StockItemData;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/stock")
+public class StockItemController {
+    private StockItemData stockData;
+
+    @Autowired
+    public StockItemController(StockItemData stockData) {
+        this.stockData = stockData;
+    }
+
+    @GetMapping
+    public List<StockItem> getAllStockItems() {
+        return stockData.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<StockItem> getStockItemById(@PathVariable Long stockID) {
+        Optional<StockItem> stockItemOptional = stockData.findById(stockID);
+        return stockItemOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<StockItem> createStockItem(@RequestBody StockItem stockItem) {
+        StockItem createdStockItem = stockData.save(stockItem);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdStockItem);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<StockItem> updateStockItem(@PathVariable Long stockID, @RequestBody StockItem updatedStockItem) {
+        Optional<StockItem> existingStockItemOptional = stockData.findById(stockID);
+        if (existingStockItemOptional.isPresent()) {
+            StockItem existingStockItem = existingStockItemOptional.get();
+            existingStockItem.setStockID(stockID);  // Ensure that the stockID is set properly
+            existingStockItem.setProductName(updatedStockItem.getProductName());
+            existingStockItem.setProductBrand(updatedStockItem.getProductBrand());
+            existingStockItem.setStockPrice(updatedStockItem.getStockPrice());
+            existingStockItem.setStockColour(updatedStockItem.getStockColour());
+            existingStockItem.setStockSize(updatedStockItem.getStockSize());
+            existingStockItem.setStockQty(updatedStockItem.getStockQty());
+            StockItem updated = stockData.save(existingStockItem);
+            return ResponseEntity.ok(updated);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteStockItem(@PathVariable Long stockID) {
+        Optional<StockItem> existingStockItemOptional = stockData.findById(stockID);
+        if (existingStockItemOptional.isPresent()) {
+            stockData.deleteById(stockID);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
